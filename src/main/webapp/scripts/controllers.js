cc91737379019c36e26bd8101098cf74ad25e7f0
John Shultz <jshultz@thelathe.com>
2014-05-21 16:24:57 -0400

    updates to draggable

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -4,6 +4,36 @@
 
 johnGShultzApp.controller('MainController', ['$scope',
     function ($scope) {
+      $scope.gallery = [
+      { path: "../images/2500px-NYC_subway-4D_lowres.jpg" },
+      { path: "../images/bike.jpg" },
+      { path: "../images/downtown.jpg" },
+      { path: "../images/horses.jpg" },
+      { path: "../images/lady_liberty.jpg" },
+      { path: "../images/limelight_100.jpg" },
+      { path: "../images/snowshoe.jpg" }
+      ];
+      for (var i = 0; i < $scope.gallery.length; i++) {
+        $scope.gallery[i].d = randomN(true)+"deg";
+        $scope.gallery[i].x = randomN(false);
+        $scope.gallery[i].y = randomN(false);
+        $scope.gallery[i].desktopStyle="left:"+$scope.gallery[i].x+"px; \
+        top:"+$scope.gallery[i].y+"px; \
+        -ms-transform: rotate("+$scope.gallery[i].d+"); \
+        -webkit-transform: rotate("+$scope.gallery[i].d+"); \
+        transform: rotate("+$scope.gallery[i].d+");";
+      }
+      setTimeout(function(){
+        $( ".photo " ).draggable();
+      }, 1000);
+      function randomN(negValues) {
+        var n = Math.floor((Math.random() * 100) + 1);
+        if (n > 50 && negValues) {
+          n = n * -1;
+        }
+        n = (n / 3);
+        return n;
+      }
     }]);
 
 johnGShultzApp.controller('AdminController', ['$scope',
@@ -225,9 +255,8 @@ johnGShultzApp.controller('AuditsController', ['$scope', '$translate', '$filter'
 
         $scope.today();
         $scope.previousMonth();
-        
+
         AuditsService.findByDates($scope.fromDate, $scope.toDate).then(function(data){
             $scope.audits = data;
         });
     }]);
-
